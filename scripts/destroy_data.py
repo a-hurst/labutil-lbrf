ddee70e0d53241d545468106e6e6411593ca6e71
Austin Hurst <mynameisaustinhurst@gmail.com>
2026-04-02 22:28:55 -0300

    Fix destroy_data script

--- scripts/destroy_data.py
+++ scripts/destroy_data.py
@@ -1,15 +1,18 @@
 import os
+import sys
 import shutil
-from click import confirm, secho
+from click import prompt, secho
 
 
 def err(txt):
     secho("Error: " + txt, fg="red", bold=True)
-    os.exit(-1)
+    print("\nAborting...")
+    sys.exit(1)
 
 
 # Get database paths and ensure backup db exists
 
+print("")
 if not os.path.exists("ExpAssets"):
     err("Not a valid klibs project, unable to remove data!")
 
@@ -31,7 +34,7 @@ for item in os.listdir("ExpAssets"):
             db_names.add(basename)
 
 if not len(db_names) == 1:
-    err("Multiple databases in ExpAssets, aborting: " + str(list(db_names)))
+    err("Inconsistent database names in ExpAssets: " + str(list(db_names)))
 
 if not backup_path:
     err("No backup database exists, unable to remove data!")
@@ -43,18 +46,18 @@ if not db_path:
 # Once we're sure everything's okay, remove the database and replace with the backup
 
 secho(
-    "Note: This will permanently delete all data from the last participant!",
-    fg="red"
+    "Note: This will permanently delete all data from the last participant!\n",
+    fg="red", bold=True
 )
 
-resp = confirm(
+resp = prompt(
     "Press 'y' to confirm, or any other key to exit",
     default='n', show_default=False
 )
-print("")
+print("\n")
 if resp == 'y':
     os.remove(db_path)
     shutil.copyfile(backup_path, db_path)
-    secho("Data from last participant destroyed successfully!", fg="green")
+    secho("Data from last participant destroyed successfully!")
 else:
     secho("Aborted, no changes made.")
